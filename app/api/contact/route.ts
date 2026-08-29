5c42a5ddf93b782f5462251528bae547ba9ed567
maciooox112 <maciooox112@gmail.com>
2026-08-29 18:13:49 +0200

    mail final fix

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -118,7 +118,7 @@ export async function POST(request: Request) {
     
     await resend.emails.send({
       from: 'Formularz Kontaktowy <powiadomienia@pracowniamegan.com>',
-      to: 'maciooox112@gmail.com',
+      to: 'pracowniamegan@gmail.com',
       replyTo: email,
       subject: `✉️ [Formularz] ${title || 'Zapytanie o kurs'} – ${name}`,
       text: `Imię i nazwisko: ${name}\nE-mail klienta: ${email}\nTemat: ${title || 'Zapytanie o kurs'}\n\nTreść wiadomości:\n${message}`,
